a1c111b4d033e849134fe3dcab71107bf2a6c88a
PabloDeLaCruz1 <pablo.de.la.cruz.pro@gmail.com>
2022-07-08 08:50:32 -0400

    data structures, algorithms

--- CodingFoundamentalsTests/CodingFoundamentalsTests.swift
+++ CodingFoundamentalsTests/CodingFoundamentalsTests.swift
@@ -71,10 +71,10 @@ class CodingFundamentalsTests: XCTestCase {
         //Repeating Digits
         XCTAssertEqual(Fundamentals().bubbleSort([1, 1, 1, 1]), [1, 1, 1, 1])
 
-        //1 Digit
+        // 1 Digit
         XCTAssertEqual(Fundamentals().bubbleSort([1]), [1])
 
-        // 6Digits
+        // 6 Digits
         XCTAssertEqual(Fundamentals().bubbleSort([1, 3, 6, 2, 4, 5]), [1, 2, 3, 4, 5, 6])
 
         // Reversing
@@ -84,4 +84,88 @@ class CodingFundamentalsTests: XCTestCase {
         XCTAssertEqual(Fundamentals().bubbleSort([50, 20, 15, 999, 111]), [15, 20, 50, 111, 999])
     }
 
+    func testFibonacci() {
+        XCTAssertEqual(Fundamentals().fibonacci(n: 0), 0)
+        XCTAssertEqual(Fundamentals().fibonacci(n: 5), 5)
+        XCTAssertEqual(Fundamentals().fibonacci(n: 10), 55)
+        XCTAssertEqual(Fundamentals().fibonacci(n: 20), 6765)
+    }
+
+    func testFactorial () {
+        XCTAssertEqual(Fundamentals().factorial(n: 0), 1)
+        XCTAssertEqual(Fundamentals().factorial(n: 5), 120)
+        XCTAssertEqual(Fundamentals().factorial(n: 10), 3628800)
+        XCTAssertEqual(Fundamentals().factorial(n: 20), 2432902008176640000)
+
+    }
+
+    func testSecondLargest () {
+        XCTAssertEqual(Fundamentals().secondLargest(arr: [0, 2, 4, 12, 52]), 12)
+        XCTAssertEqual(Fundamentals().secondLargest(arr: [10, 222, 134, 12, 52]), 134)
+        XCTAssertEqual(Fundamentals().secondLargest(arr: [0, 1]), 0)
+    }
+
+    func testIsPrime () {
+        XCTAssertTrue(Fundamentals().isPrime(2))
+        XCTAssertTrue(Fundamentals().isPrime(3))
+        XCTAssertTrue(Fundamentals().isPrime(11))
+        XCTAssertFalse(Fundamentals().isPrime(4))
+        XCTAssertFalse(Fundamentals().isPrime(120))
+    }
+
+    func testFlattenAndSort() {
+        //Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+        XCTAssertEqual(Fundamentals().flattenAndSort([[Int]]()), [])
+        XCTAssertEqual(Fundamentals().flattenAndSort([[Int](), []]), [])
+        XCTAssertEqual(Fundamentals().flattenAndSort([[], [1]]), [1])
+        XCTAssertEqual(Fundamentals().flattenAndSort([[], [], [], [2], [], [1]]), [1, 2])
+        XCTAssertEqual(Fundamentals().flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]), [1, 2, 3, 4, 5, 6, 7, 8, 9])
+        XCTAssertEqual(Fundamentals().flattenAndSort([[1, 3, 5], [100], [2, 4, 6]]), [1, 2, 3, 4, 5, 6, 100])
+        XCTAssertEqual(Fundamentals().flattenAndSort([[111, 999], [222], [333], [444], [888], [777], [666], [555]]), [111, 222, 333, 444, 555, 666, 777, 888, 999])
+    }
+
+    func testEncryptThis() {
+        XCTAssertEqual(Fundamentals().encryptThis(text: "A wise old owl lived in an oak"), "65 119esi 111dl 111lw 108dvei 105n 97n 111ka")
+        XCTAssertEqual(Fundamentals().encryptThis(text: "The more he saw the less he spoke"), "84eh 109ero 104e 115wa 116eh 108sse 104e 115eokp")
+        XCTAssertEqual(Fundamentals().encryptThis(text: "The less he spoke the more he heard"), "84eh 108sse 104e 115eokp 116eh 109ero 104e 104dare")
+        XCTAssertEqual(Fundamentals().encryptThis(text: "Why can we not all be like that wise old bird"), "87yh 99na 119e 110to 97ll 98e 108eki 116tah 119esi 111dl 98dri")
+        XCTAssertEqual(Fundamentals().encryptThis(text: "Thank you Piotr for all your help"), "84kanh 121uo 80roti 102ro 97ll 121ruo 104ple")
+    }
+    func testDigits() {
+        XCTAssertEqual(Fundamentals().digits(5), 1)
+        XCTAssertEqual(Fundamentals().digits(12345), 5)
+        XCTAssertEqual(Fundamentals().digits(9876543210), 10)
+    }
+    
+    func testMyStack() {
+        var aStack = Fundamentals.myStack<Int>()
+        
+        aStack.push(1)
+        XCTAssertEqual(aStack.pop(), 1)
+        aStack.push(23)
+        XCTAssertEqual(aStack.pop(), 23)
+        aStack.push(121)
+        XCTAssertEqual(aStack.pop(), 121)
+    }
+    
+    func testMyQueue() {
+        var queue = Fundamentals.myQueue<String>()
+        queue.enqueue("Mark")
+        XCTAssertEqual(queue.dequeue() , "Mark")
+
+        queue.enqueue("Steve")
+        XCTAssertEqual(queue.dequeue() , "Steve")
+        
+        queue.enqueue("Kim")
+        XCTAssertEqual(queue.dequeue(), "Kim")
+       
+        XCTAssertNil(queue.dequeue())
+    }
+
+    func testBinarySearch() {
+        let numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67]
+
+      XCTAssertEqual(Fundamentals().binarySearch(numbers, 43, 0 ..< numbers.count), 13)
+    }
 }
--- CodingFundamentals/Fundamentals.swift
+++ CodingFundamentals/Fundamentals.swift
@@ -1,79 +1,178 @@
 import Foundation
 
 class Fundamentals {
-    
+
     // #1 Create a function that adds two numbers and returns that value
     func addNumbers(x: Int, y: Int) -> Int {
         //Your code here
-        
+
         return 0
     }
-    
+
     // #2 Create a function that subtracts two numbers and returns that value
-    func subNumbers(x: Int, y: Int) -> Int {		    
+    func subNumbers(x: Int, y: Int) -> Int {
         //Your code here
-        
+
         return 0
     }
-    
+
     // #3 Create a function that multiplies two numbers and returns that value
     func multNumbers(x: Int, y: Int) -> Int {
         //Your code here
-        
+
         return 0
     }
 
     // #4 Create a function that checks if a String is a Palindrome
-    // A palindrome is a word, number, phrase or other sequence that reads the same forward as it reads backward. 
+    // A palindrome is a word, number, phrase or other sequence that reads the same forward as it reads backward.
     func isPalindrome(word: String) -> Bool {
         //Your code here
-        
+
         return false
     }
-    
+
     // #5 Find the number of occurrences of a character in a String?
-    func charCounter(char: Character, string: String) -> Int{
+    func charCounter(char: Character, string: String) -> Int {
         //Your code here
-        
+
         return 0
     }
-    
+
     // #6 Find a matching Item in an Array
     func matchItemsInArray(item: String, arr: [String]) -> [String] {
         //Your code here
-        
+
         return [""]
     }
-    
+
     // #7 Complete the square sum function so that it squares each number passed into it and then sums the results together.
     func squareSum(_ vals: [Int]) -> Int {
-      //Your code here
-        
+        //Your code here
+
         return 0
     }
-    
+
     // #8 When provided with a number between 0-9, return it in words.
     func switchItUp(_ number: Int) -> String {
         //Your code here
-        
+
         return ""
     }
 
     // #9 A hero is going to fight a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive? Return True if yes, False otherwise :)
     func hero(bullets: Int, dragons: Int) -> Bool {
-      //Your code here
-        
+        //Your code here
+
         return false
     }
-    
+
     // #10 Implement Bubble Sort
     // Bubble Sort is a sorting algorithm where the largest items bubble up one at a time
     func bubbleSort(_ arr: [Int]) -> [Int] {
         //Your code here
-        
+
         return []
+    }
+
+    // #11 Fibonacci series using recursion
+    //The Fibonacci numbers are the numbers in the following integer sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ……..
+    //Given a number, print the corresponding integer in the series.
+    func fibonacci(n: Int) -> Int {
+        //Your code here
+
+        return 0
+    }
+
+    // #12  Find the factorial of an integer
+    // A factorial is a function that multiplies a number by every number below it. For example, 5! = 5*4*3*2*1=120. "!" is the factorio symbol.
+    func factorial(n: Int) -> Int {
+        //Your code here
+
+        return 0
+    }
+
+    // #13 Find the second largest number in an array
+    func secondLargest(arr: [Int]) -> Int {
+        //Your code here
+
+        return 0
+    }
+
+    // #14 Check if the given number is prime
+    func isPrime(_ n: Int) -> Bool {
+        //Your code here
+
+        return false
+    }
+
+    // #15 Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+
+    func flattenAndSort<T: Comparable>(_ arr: [[T]]) -> [T] {
+        //Your code here
+
+        return []
+    }
+
+    // #16 You need to encrypt each word in the message using the following rules:
+    //  1. The first letter must be converted to its ASCII code.
+    //  2. The second letter must be switched with the last letter
+    func encryptThis(text: String) -> String {
+        //Your code here
+
+        return ""
+    }
+
+    // #17 Determine the total number of digits in the integer
+    func digits(_ n: Int) -> Int {
+        //Your code here
+
+        return 0
+    }
+
+    // #18 Implement a basic Stack Data Structure for Ints
+    struct myStack<Int> {
+        var array = [Int]()
+
+        mutating func push(_ n: Int) {
+            //your code here
+
+        }
+
+        mutating func pop() -> Int? {
+            //your code here
+
+            return (0 as! Int)
+        }
+
+    }
+    // #19 Implement a basic Queue Data Structure for all types
+
+    struct myQueue<T> {
+        var array = [T]()
+        var count: Int {
+            return array.count
+        }
+        var isEmpty: Bool {
+            return array.isEmpty
+        }
+
+        public mutating func enqueue(_ element: T) {
+            //your code here
+
+        }
+
+        public mutating func dequeue() -> T? {
+            //your code here
+
+            return ("" as! T)
+        }
+    }
+
+    // #20 Implement a Binary Search algorithm for Ints
+    func binarySearch(_ numbers: [Int], _ key: Int, _ range: Range<Int>) -> Int {
+        //your code here
+
+        return 0
     }
     
 }
-
-
